5ff904b25384cc6b778e5fccc6935caa17ad1b04
NikolayRag <nikolayr@mail.ru>
2021-06-11 19:42:52 +0300

    add init code
    Based on https://github.com/mathijsk/Arduino-Library-for-ATEM-Switchers

--- ATEM-remote.ino
+++ ATEM-remote.ino
@@ -1,71 +1,45 @@
 /*
- * 
- * 
+Atem helper
+
+Based on https://github.com/mathijsk/Arduino-Library-for-ATEM-Switchers
+
+To use https://github.com/UIPEthernet/UIPEthernet library (with ENC28j60 chip)
+make EthernetUDP.h alias for provided UIPUdp.h
  */
- 
+
+
+#include <SPI.h> 
 #include <Ethernet.h>
 
+#include <ATEM.h>
 
-byte mac[] = {0x00,0x01,0x02,0x03,0x04,0x05};
+
+byte mac[] = {0x90, 0xA2, 0xDA, 0x0D, 0x6B, 0xB9};
 IPAddress selfIp(192, 168, 1, 222);
 IPAddress atemIp(192, 168, 1, 241);
 
-
-
-EthernetUDP UDP;
-unsigned long Timeout;
+ATEM AtemSwitcher(atemIp,56789);
 
 
 void setup() {
-  Ethernet.begin(mac,selfIp);
+  Serial.begin(115200);
 
-  Serial.begin(9600);
-  Serial.print("init\n");
 
-  Timeout = millis()+1000;
+  Ethernet.begin(mac, selfIp);
+
+  AtemSwitcher.serialOutput(0x80);
+  AtemSwitcher.connect();
+
+
+  Serial.print("Init ok\n");
 }
 
+
 void loop() {
-  int success;
 
-  if (millis()>Timeout){
+  AtemSwitcher.runLoop();
 
-      Serial.print("\nConnect");
-      do {
-          success = UDP.beginPacket(atemIp,5000);
-          Serial.print(success ? " ok\n" : " fail\n");
-      } while (!success && (millis()<Timeout));
-      
-      if (success) {
-        success = UDP.write("hello world from arduino");
-        Serial.print("Sent "); Serial.print(success); Serial.print(" bytes, ");
-
-        success = UDP.endPacket();
-        Serial.print(success ? "ok\n" : "fail\n");
-
-        do {
-          success = UDP.parsePacket();
-          Serial.print(".");
-        } while (!success && (millis()<Timeout));
-        
-        Serial.print(success ? " parse ok\n" : " parse none\n");
-
-
-        if (success) {
-          Serial.print(F("received '")); Serial.print(UDP.available()); Serial.print("b:\n");
-
-          while (UDP.available()) {
-            char c = UDP.read();
-            Serial.print(c);
-          }
-          
-
-          UDP.flush();
-        }
-        
-      }
-
-      UDP.stop();
-      Timeout = millis()+1000;
-    }
+  
+  Serial.print(".\n");
+  delay(500);
 }
